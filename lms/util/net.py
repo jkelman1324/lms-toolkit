1c8b82c6ee8ffb9963cbaf3ded16b680966858dd
Jake Kelman <j.d.kelman95@gmail.com>
2026-08-02 00:07:04 -0700

    Type check fixes.

--- lms/util/net.py
+++ lms/util/net.py
@@ -126,10 +126,10 @@ def clean_blackboard_response(response: requests.Response, body: str) -> str:
     body = _clean_base_response(response, body)
 
     # Work on both request and response headers.
-    for headers in [response.headers, response.request.headers]:
+    for headers in [response.headers, response.request.headers]:  # type: ignore[attr-defined]
         for key in list(headers.keys()):
             if (key.strip().lower() in BLACKBOARD_CLEAN_REMOVE_HEADERS):
-                headers.pop(key, None)
+                headers.pop(key, None)  # type: ignore[attr-defined]
 
     # Most blackboard responses are JSON.
     try:
@@ -254,9 +254,9 @@ def clean_moodle_response(response: requests.Response, body: str) -> str:
 
     # Work on both request and response headers.
     for headers in [response.headers, response.request.headers]:
-        for key in list(headers.keys()):
+        for key in list(headers.keys()):  # type: ignore[attr-defined]
             if (key.strip().lower() in MOODLE_CLEAN_REMOVE_HEADERS):
-                headers.pop(key, None)
+                headers.pop(key, None)  # type: ignore[attr-defined]
 
     # Remove Chunking
     response.headers.pop('transfer-encoding', None)
